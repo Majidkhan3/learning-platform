24a7eade35df545606b7e123f6007c71f1716c43
Majidkhan3 <majidabid017@gmail.com>
2025-05-16 19:29:28 +0500

    changes

--- src/app/(admin)/dashboards/portugais/page.jsx
+++ src/app/(admin)/dashboards/portugais/page.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import PageTitle from '@/components/PageTitle';
+import { Card, CardHeader, Col, Row } from 'react-bootstrap';
 
-const page = () => {
-  return (
-    <div>
-      portugal
-    </div>
-  )
-}
+import PageWithFilters from '@/app/(admin)/dashboards/espagnol/components/espagnol/PageWithFilters';
 
-export default page
+import IconifyIcon from '@/components/wrappers/IconifyIcon';
+import { useAuth } from '@/components/wrappers/AuthProtectionWrapper';
+export const metadata = {
+  title: 'Portugais',
+  // description: 'Espagnol',
+};
+const AnalyticsPage = () => {
+  return <>
+      <PageTitle title="Espagnol" subName="Dashboard" />
+      {/* <Statistics />
+      <Row>
+        <SalesChart />
+        <BalanceCard />
+      </Row>
+      <SocialSource />
+      <Transaction /> */}
+      <Row>
+        <Col lg={12}>
+          <Card>
+            <CardHeader className="border-0">
+              <Row className="justify-content-between">
+                <Col lg={6}>
+                  <Row className="align-items-center">
+                    <Col lg={6}>
+                      <form className="app-search d-none d-md-block me-auto">
+                        <div className="position-relative">
+                          <input type="search" className="form-control" placeholder="Rechercher un mot..." autoComplete="off" />
+                          <IconifyIcon icon="solar:magnifer-broken" className="search-widget-icon" />
+                        </div>
+                      </form>
+                    </Col>
+                    {/* <Col lg={4}>
+                      <h5 className="text-dark fw-medium mb-0">
+                        311 <span className="text-muted"> Agent</span>
+                      </h5>
+                    </Col> */}
+                  </Row>
+                </Col>
+                <Col lg={6}>
+                  <div className="text-md-end mt-3 mt-md-0">
+                    <a href="/dashboards/espagnol/tags">
+
+                    <button type="button"  className="btn btn-outline-primary me-2">
+                      <IconifyIcon icon="ri:settings-2-line" className="me-1" />
+                      Gérer les tags
+                    </button>
+                    </a>
+                    <a href="/dashboards/espagnol/word">
+                    <button type="button" className="btn btn-outline-primary me-2">
+                      <IconifyIcon icon="ri:add-line" className="me-1" /> Ajouter un mot
+                    </button>
+                    </a>
+                    <a href="/dashboards/espagnol/add-multiple-words">
+                    <button type="button" className="btn btn-success me-1">
+                      <IconifyIcon icon="ri:add-line" /> Ajouter plusieurs mots
+                    </button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </CardHeader>
+          </Card>
+        </Col>
+      </Row>
+      <PageWithFilters/>
+    </>;
+};
+export default AnalyticsPage;
