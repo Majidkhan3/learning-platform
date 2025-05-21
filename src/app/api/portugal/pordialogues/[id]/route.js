d5a8874833d8484416b8c072b6067d7bb55672fa
harisali-007 <alisarhm570@gmail.com>
2025-05-21 17:15:10 +0500

    make french dashboard

---
+++ src/app/api/portugal/pordialogues/[id]/route.js
@@ -0,0 +1,34 @@
+// pages/api/dialogues/[id].js
+import Pordialgues from '../../../../../model/Pordialogue'
+import { NextResponse } from 'next/server'
+export async function GET(req, { params }) {
+  try {
+    const { id } = params
+    console.log('Fetching dialogue with ID:', id)
+    const dialogue = await Pordialgues.findById(id)
+    if (!dialogue) {
+      return new NextResponse(JSON.stringify({ error: 'Dialogue not found' }), { status: 404 })
+    }
+    return new NextResponse(JSON.stringify(dialogue), { status: 200 })
+  } catch (error) {
+    console.error('Error fetching dialogue:', error)
+    return new NextResponse(JSON.stringify({ error: 'Error fetching dialogue' }), { status: 500 })
+  }
+}
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = params
+    console.log('Deleting dialogue with ID:', id)
+
+    const deletedDialogue = await Pordialgues.findByIdAndDelete(id)
+
+    if (!deletedDialogue) {
+      return new NextResponse(JSON.stringify({ error: 'Dialogue not found' }), { status: 404 })
+    }
+
+    return new NextResponse(JSON.stringify({ message: 'Dialogue deleted successfully' }), { status: 200 })
+  } catch (error) {
+    console.error('Error deleting dialogue:', error)
+    return new NextResponse(JSON.stringify({ error: 'Error deleting dialogue' }), { status: 500 })
+  }
+}
---
+++ src/model/Frdialogue.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const dialogueSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
+  },
+  url: {
+    type: String,
+    required: false,
+  },
+  dialogue: {
+    // Single dialogue string
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Frdialogue = mongoose.models.Frdialogue || mongoose.model('Frdialogue', dialogueSchema)
+
+export default Frdialogue
