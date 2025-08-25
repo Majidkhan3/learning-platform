ca75da4e26f870d82337e7faa71e70689bc155bd
harisali-007 <alisarhm570@gmail.com>
2025-08-25 16:27:45 +0500

    fixed errors

--- src/components/layout/TopNavigationBar/page.jsx
+++ src/components/layout/TopNavigationBar/page.jsx
@@ -14,10 +14,10 @@ const page = () => {
             <div className="d-flex align-items-center gap-2">
               <LeftSideBarToggle />
               <form className="app-search d-none d-md-block me-auto">
-                <div className="position-relative">
+                {/* <div className="position-relative">
                   <input type="search" className="form-control border-0" placeholder="Search..." autoComplete="off" />
                   <IconifyIcon icon="ri:search-line" className=" search-widget-icon" />
-                </div>
+                </div> */}
               </form>
             </div>
             <div className="d-flex align-items-center gap-1">
